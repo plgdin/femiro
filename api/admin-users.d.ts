e8b87523f438bb154cd2153d4bd29484457d49e8
brolol <anshajshaji3@gmail.com>
2026-08-07 00:50:08 +0530

    chore: remove compiled conflicting js and d.ts files from build output

--- api/admin-users.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function handleAdminUsers(req: any, body?: any): Promise<{
-    status: number;
-    data: unknown;
-}>;
-export default function handler(req: any, res: any): Promise<any>;
--- api/verify-payment.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function handleVerifyPayment(req: any, body: any): Promise<{
-    status: number;
-    data: {
-        success: boolean;
-        order_id: any;
-        payment_id: any;
-    };
-}>;
-export default function handler(req: any, res: any): Promise<any>;
